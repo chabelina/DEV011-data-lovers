4de6cb92031054d75c5cb79eb3df007e5246ecea
Elizabeth <chabelina2003@gmail.com>
2023-09-26 12:52:09 -0500

    Developed renderItems function for the dinamic visualization of the data

--- src/view.js
+++ src/view.js
@@ -1,6 +1,17 @@
 // estas funciones son de ejemplo
 
-export const renderItems = () => {
-  return 'example';
-};
+export const renderItems = (data) => {
+  const ul = document.getElementById("database-list");
+  ul.innerHTML = ""; // Clear the list before populating it
 
+  data.forEach((item) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+          <strong>${item.title}</strong>
+          <p>${item.description}</p>
+          <p>Producer: ${item.producer}</p>
+        `;
+    ul.appendChild(li);
+  });
+  return ul.innerHTML
+};
