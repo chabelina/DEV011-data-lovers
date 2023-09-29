ee9eaa2182bddd046fa0812961f1d0f21d08e59f
Elizabeth <123765828+chabelina@users.noreply.github.com>
2023-09-29 13:19:03 -0500

    [FEATURE] User Interface  (#2)
    * added a navegation bar with filter and order selectors
    
    * fixing navegation bar/filter and sort interface
    
    * Developed renderItems function for the dinamic visualization of the data
    
    * Added CSS styling to the cards and responsive flexbox
    
    * Added page footer and its CSS styling
    
    * Fixed the cards' width styling in CSS
    
    * Changing footer height by fixing it's padding to 60px
    
    * Modified the filter and sort UI (removed one of the cleaning buttons and added labels for intuitive use of selection bars)
    
    * Fixing the last details of the navigation bar (CSS styling for the clear button and labels + elements display correction)
    
    * Removed hover style from the clean button
    
    ---------
    
    Co-authored-by: Josefa Fontecilla <fontecillajosefa10@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import { example } from './dataFunctions.js';
+//import { example } from './dataFunctions.js';
 import { renderItems } from './view.js';
 
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+//console.log('DAT GHIBLI: ', data);
 
-console.log(example, renderItems, data);
+renderItems(data.films);
--- src/view.js
+++ src/view.js
@@ -1,6 +1,19 @@
 // estas funciones son de ejemplo
 
-export const renderItems = () => {
-  return 'example';
-};
+export const renderItems = (data) => {
+  const ul = document.getElementById("database-list");
+  ul.innerHTML = ""; // Clear the list before populating it
 
+  data.forEach((item) => {
+    const li = document.createElement("li");
+    li.classList.add("cards");
+    
+    li.innerHTML = `
+          <img src= "${item.poster}" width="203" height="290">
+          <h3>${item.title}</h3>
+          <p>(${item.release_date})</p>
+        `;
+    ul.appendChild(li);
+  });
+  return ul.innerHTML
+};
