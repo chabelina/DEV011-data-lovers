2ebc783f8ee12621fc010baefc8278ddb42abb0b
Elizabeth <chabelina2003@gmail.com>
2023-10-03 11:03:23 -0500

    Beginning filterByGender implementation

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -5,6 +5,17 @@ export const filterData = (data, filterBy, value) => {
   return filteredMovies;
 };
 
-export const anotherExample = () => {
+
+
+export const filterByGender = (data, filterBy, value) => {
+  data.films.forEach((film) => {
+    // Access and manipulate the first character's "gender" data
+    const firstCharacterGender = film.people[0].gender;
+
+    // Log or manipulate the data as needed
+    console.log(
+      `Film: ${film.title}, Protagonist Gender: ${firstCharacterGender}`
+    );
+  });
   return [];
 };
