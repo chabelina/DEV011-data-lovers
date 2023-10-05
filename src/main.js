34050ef637e8eca4ca56ece7e236783c2e2241b0
jofontea <142179255+jofontea@users.noreply.github.com>
2023-10-05 12:02:19 -0300

    [FIX] Fixing navegation bar details (CSS & HTML) (#4)
    * Implemented sortData function and began the development of the clearButton function
    
    * Changed position of the navbar's clearing button and added a label to the order selection
    
    ---------
    
    Co-authored-by: Elizabeth <chabelina2003@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { filterData } from "./dataFunctions.js";
 import { renderItems } from "./view.js";
+import { filterData } from "./dataFunctions.js";
 import { filterByGender } from "./dataFunctions.js";
+import { sortData } from "./dataFunctions.js";
 //import { renderFilteredMovies } from "./view.js";
 
 import data from "./data/ghibli/ghibli.js";
@@ -8,6 +9,7 @@ import data from "./data/ghibli/ghibli.js";
 //console.log('DAT GHIBLI: ', data);
 
 renderItems(data.films);
+
 const directorOptions = document.querySelector("#director-options");
 
 directorOptions.addEventListener("change", () => {
@@ -30,3 +32,23 @@ genderOptions.addEventListener("change", () => {
   console.log(filteredMovies);
   renderItems(filteredMovies);
 });
+
+const sortOptions = document.querySelector("#sort-order");
+
+sortOptions.addEventListener("change", () => {
+  const sortedMovies = sortData(
+    data.films, 
+    "rt_score", 
+    sortOptions.value
+  );
+  console.log(sortedMovies);
+  renderItems(sortedMovies);
+});
+
+const clearButton = document.querySelector("#button-clear");
+
+clearButton.addEventListener("click", () => {
+  renderItems(data.films);
+});
+
+
