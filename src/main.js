42ead7f4f89fc3314779890d7921e1785a680a50
Elizabeth <123765828+chabelina@users.noreply.github.com>
2023-10-11 15:37:19 -0500

    Implemented the clear button functionality and developed dynamic accumulation of filter and sort options (#9)

--- src/main.js
+++ src/main.js
@@ -1,57 +1,71 @@
 import { renderItems } from "./view.js";
-import { filterData } from "./dataFunctions.js";
-import { filterByGender } from "./dataFunctions.js";
-import { sortData } from "./dataFunctions.js";
-import { computeStats } from "./dataFunctions.js";
-//import { renderFilteredMovies } from "./view.js";
+import {
+  filterData,
+  filterByGender,
+  sortData,
+  computeStats,
+} from "./dataFunctions.js";
 
 import data from "./data/ghibli/ghibli.js";
 
 //console.log('DAT GHIBLI: ', data);
 
 const nonHumanSpeciesPercent = computeStats(data.films);
-
 renderItems(data.films, nonHumanSpeciesPercent);
 
+let filters = {
+  director: null,
+  gender: null,
+  sortOrder: null,
+};
+
+function applyFilters() {
+  let filteredMovies = [...data.films];
+
+  if (filters.director) {
+    filteredMovies = filterData(filteredMovies, "director", filters.director);
+  }
+
+  if (filters.gender) {
+    filteredMovies = filterByGender(filteredMovies, "gender", filters.gender);
+  }
+
+  if (filters.sortOrder) {
+    filteredMovies = sortData(filteredMovies, "rt_score", filters.sortOrder);
+  }
+
+  renderItems(filteredMovies, nonHumanSpeciesPercent);
+}
+
 const directorOptions = document.querySelector("#director-options");
-
 directorOptions.addEventListener("change", () => {
-  const filteredMovies = filterData(
-    data.films,
-    "director",
-    directorOptions.value
-  );
-  renderItems(filteredMovies);
+  filters.director = directorOptions.value;
+  applyFilters();
 });
 
+// Evento para el filtro de género
 const genderOptions = document.querySelector("#gender-options");
-
 genderOptions.addEventListener("change", () => {
-  const filteredMovies = filterByGender(
-    data.films,
-    "gender",
-    genderOptions.value
-  );
-  //console.log(filteredMovies);
-  renderItems(filteredMovies);
+  filters.gender = genderOptions.value;
+  applyFilters();
 });
 
+// Evento para el filtro de orden
 const sortOptions = document.querySelector("#sort-order");
-
 sortOptions.addEventListener("change", () => {
-  const sortedMovies = sortData(
-    data.films, 
-    "rt_score", 
-    sortOptions.value
-  );
-  //console.log(sortedMovies);
-  renderItems(sortedMovies);
+  filters.sortOrder = sortOptions.value;
+  applyFilters();
 });
 
+// Evento para el botón de limpiar
 const clearButton = document.querySelector("#button-clear");
-
 clearButton.addEventListener("click", () => {
-  renderItems(data.films);
+  // Restablecer los filtros
+  filters = {
+    director: null,
+    gender: null,
+    sortOrder: null,
+  };
+
+  renderItems(data.films, nonHumanSpeciesPercent);
 });
-
-
